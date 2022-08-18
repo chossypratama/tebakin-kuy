f375192f0d38ded8dd6c24e3bd53a83bb009741b
fadilahagiel <fadilahagiel@gmail.com>
2022-08-18 17:30:37 +0700

    pertanyaan dan algoritma

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 let listPertanyaan = {
     penyanyi: [
         {
-            pertanyaan: 'pertanyaan1', jawaban: [
+            pertanyaan: 'pertanyaan1',
+            jawaban: [
                 { value: "benar", text: "jwb1" },
                 { value: "salah", text: "jwb2" },
                 { value: "salah", text: "jwb3" },
@@ -9,7 +10,8 @@ let listPertanyaan = {
             ]
         },
         {
-            pertanyaan: 'pertanyaan2', jawaban: [
+            pertanyaan: 'pertanyaan2',
+            jawaban: [
                 { value: "salah", text: "jwb1" },
                 { value: "benar", text: "jwb2" },
                 { value: "salah", text: "jwb3" },
@@ -77,4 +79,45 @@ let listPertanyaan = {
             ]
         }
     ]
+}
+
+// let penyanyi diklik buka pertanyaan - pertanyaan dari listPertanyaan.penyanyi.pertanyaan
+function penyanyi() {
+    let point
+    for (let i = 0; i < listPertanyaan.penyanyi.length; i++) {
+        point = 0
+        const pertanyaan = listPertanyaan.penyanyi[i];
+        if (value === 'benar') {
+            point += 1
+        }
+    }
+    let score = point / listPertanyaan.penyanyi[i]
+
+}
+// let hewan diklik buka pertanyaan - pertanyaan dari listPertanyaan.hewan.pertanyaan
+function hewan() {
+    let point
+    for (let i = 0; i < listPertanyaan.hewan.length; i++) {
+        point = 0
+        const pertanyaan = listPertanyaan.hewan[i];
+        if (value === 'benar') {
+            point += 1
+        }
+    }
+    let score = point / listPertanyaan.penyanyi[i]
+
+}
+
+// let buah diklik buka pertanyaan - pertanyaan dari listPertanyaan.buah.pertanyaan
+function buah() {
+    let point
+    for (let i = 0; i < listPertanyaan.penyanyi.length; i++) {
+        point = 0
+        const pertanyaan = listPertanyaan.penyanyi[i];
+        if (value === 'benar') {
+            point += 1
+        }
+    }
+    let score = point / listPertanyaan.penyanyi[i]
+
 }
