63ecb15127496a4a5132cb65790c708ddae18bcb
Chossy Pratama <chossy.pratama@ti.ukdw.ac.id>
2022-08-18 15:08:24 +0700

    test commit kedua

---
+++ index.js
@@ -0,0 +1,15 @@
+let pertanyaan = [
+    {
+        jokeGaring:[
+            {
+                pertanyaan: "bla bla bla?",
+                jawaban: [
+                    {value: "benar", text: "ahaha hihi"},
+                    {value: "salah", text: "ahaha hihi"},
+                    {value: "salah", text: "ahaha hihi"},
+                    {value: "salah", text: "ahaha hihi"}
+                ]
+            }
+        ]
+    }
+]
