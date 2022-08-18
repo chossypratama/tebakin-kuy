e0485c1b01612be4d6616f00aa957af07d3fcb89
fadilahagiel <fadilahagiel@gmail.com>
2022-08-18 15:11:33 +0700

    bikin list pertanyaan

--- index.js
+++ index.js
@@ -1,15 +1,82 @@
-let pertanyaan = [
-    {
-        jokeGaring:[
-            {
-                pertanyaan: "bla bla bla?",
-                jawaban: [
-                    {value: "benar", text: "ahaha hihi"},
-                    {value: "salah", text: "ahaha hihi"},
-                    {value: "salah", text: "ahaha hihi"},
-                    {value: "salah", text: "ahaha hihi"}
-                ]
-            }
-        ]
-    }
-]
+let listPertanyaan = {
+    penyanyi: [
+        {
+            pertanyaan: 'pertanyaan1', jawaban: [
+                { value: "benar", text: "jwb1" },
+                { value: "salah", text: "jwb2" },
+                { value: "salah", text: "jwb3" },
+                { value: "salah", text: "jwb4" },
+            ]
+        },
+        {
+            pertanyaan: 'pertanyaan2', jawaban: [
+                { value: "salah", text: "jwb1" },
+                { value: "benar", text: "jwb2" },
+                { value: "salah", text: "jwb3" },
+                { value: "salah", text: "jwb4" },
+            ]
+        },
+        {
+            pertanyaan: 'pertanyaan3', jawaban: [
+                { value: "salah", text: "jwb1" },
+                { value: "salah", text: "jwb2" },
+                { value: "benar", text: "jwb3" },
+                { value: "salah", text: "jwb4" },
+            ]
+        }
+
+
+    ],
+    hewan: [
+        {
+            pertanyaan: 'pertanyaan1', jawaban: [
+                { value: "benar", text: "jwb1" },
+                { value: "salah", text: "jwb2" },
+                { value: "salah", text: "jwb3" },
+                { value: "salah", text: "jwb4" },
+            ]
+        },
+        {
+            pertanyaan: 'pertanyaan2', jawaban: [
+                { value: "salah", text: "jwb1" },
+                { value: "benar", text: "jwb2" },
+                { value: "salah", text: "jwb3" },
+                { value: "salah", text: "jwb4" },
+            ]
+        },
+        {
+            pertanyaan: 'pertanyaan3', jawaban: [
+                { value: "salah", text: "jwb1" },
+                { value: "salah", text: "jwb2" },
+                { value: "benar", text: "jwb3" },
+                { value: "salah", text: "jwb4" },
+            ]
+        }
+    ],
+    buah: [
+        {
+            pertanyaan: 'pertanyaan1', jawaban: [
+                { value: "benar", text: "jwb1" },
+                { value: "salah", text: "jwb2" },
+                { value: "salah", text: "jwb3" },
+                { value: "salah", text: "jwb4" },
+            ]
+        },
+        {
+            pertanyaan: 'pertanyaan2', jawaban: [
+                { value: "salah", text: "jwb1" },
+                { value: "benar", text: "jwb2" },
+                { value: "salah", text: "jwb3" },
+                { value: "salah", text: "jwb4" },
+            ]
+        },
+        {
+            pertanyaan: 'pertanyaan3', jawaban: [
+                { value: "salah", text: "jwb1" },
+                { value: "salah", text: "jwb2" },
+                { value: "benar", text: "jwb3" },
+                { value: "salah", text: "jwb4" },
+            ]
+        }
+    ]
+}
